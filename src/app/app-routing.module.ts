3924ee6c31e09d10a3f2e07081f1cc9d127dd5eb
jamesomalley97 <44001439+jamesomalley97@users.noreply.github.com>
2019-11-23 17:57:44 +0000

    generated services component , imported modules and imported angular materials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {CreateComponent} from './create/create.component';
+import {ReadComponent} from './read/read.component';
+import { createComponent } from '@angular/compiler/src/core';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+{
+  path: 'read',
+  component:ReadComponent
+},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CreateComponent } from './create/create.component';
 import { ReadComponent } from './read/read.component';
+
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatNativeDateModule,
+  MatDatepickerModule
+  } from '@angular/material';
+  import {FormsModule} from '@angular/forms';
+  
 
 @NgModule({
   declarations: [
@@ -14,7 +29,14 @@ import { ReadComponent } from './read/read.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatNativeDateModule,
+  MatDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
